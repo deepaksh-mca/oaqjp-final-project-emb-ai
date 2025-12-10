b60d28e5e5917f1d46037940d8ba1861f45c3a4d
deepaksh-mca <deepakmca207@gmail.com>
2025-12-10 06:33:07 -0500

    code commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,12 +1,12 @@
 import requests
-import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    myobj =  { "raw_document": { "text": text_to_analyse } }
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    payload = { "raw_document": { "text": text_to_analyze } }
     
-    response = requests.post(url, json = myobj, headers = header)
+    response = requests.post(url, json=payload, headers=headers)
+    response.raise_for_status()  # Raises error if the request failed
+    
     response_json = response.json()
-
-return response_json.get("text", "")
+    return response_json
