9f52cc80b5c00ff7d5b033f392b987b57d0d8761
deepaksh-mca <deepakmca207@gmail.com>
2025-12-10 11:57:46 -0500

    code commit

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,12 +1,10 @@
 import requests
-
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     payload = { "raw_document": { "text": text_to_analyze } }    
     response = requests.post(url, json=payload, headers=headers)
-    if response.status_code == 400:
-        # Return dictionary with None values for all emotions
+    if response.status_code == 400:        
         return {
             "anger": None,
             "disgust": None,
@@ -17,13 +15,9 @@ def emotion_detector(text_to_analyze):
         }
     else:
         response.raise_for_status()
-           
     response_json = response.json()    
-    # Extract emotions from the response JSON
     emotions = response_json['emotionPredictions'][0]['emotion']    
-    # Find dominant emotion by max score
     dominant_emotion = max(emotions, key=emotions.get)    
-    # Prepare output dictionary
     formatted_output = {
         'anger': emotions.get('anger', 0),
         'disgust': emotions.get('disgust', 0),
